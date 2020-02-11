d49aad045f12af33951dde37cecb265b1885a468
saadisla <54683504+saadisla@users.noreply.github.com>
2020-02-10 23:44:10 -0500

    JSON Website
    Saad Islam Builds a website that is dynamically loaded on one page through the use of JSON to load content into a single div. Website theme based on dungeons and dragons campaign run by author.

---
+++ assignment2/app/app.js
@@ -0,0 +1,51 @@
+function trace(fName, arg){
+    //console.log(fName, arg); 
+}
+
+function initNav(){
+
+    $("a").click(function(e){
+        var cp = PROVIDER.getCurrentPageName();
+        trace("initNav ", cp);
+        var btnId = e.currentTarget.id;
+
+        if(cp!= btnId){
+            loadContent(btnId);
+        }
+
+        
+    });
+}
+
+function loadContent(pageName){
+
+    var pageContent = PROVIDER.getPageContent(pageName);
+
+    $(".content").html(pageContent);
+
+    initNav();
+}
+
+function populateNav(){
+    
+
+    var nav = PROVIDER.getMainNav();
+
+    $.each(nav, function(idx, link){
+        //console.log(link);
+        $("nav").append(`<a id="${link.path}" href="#">${link.linkName}</a>`);
+    });
+
+    loadContent("home");
+
+}
+
+function dataIsLoaded(){
+    //console.log("loaded");
+    populateNav();
+}
+
+$(document).ready(function(){
+   // alert('Hello'); 
+   PROVIDER.getData(dataIsLoaded);
+});
