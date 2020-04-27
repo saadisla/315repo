79dcea4e4ab331150c40bf9472b26a16635dda0a
saadisla <54683504+saadisla@users.noreply.github.com>
2020-04-27 01:40:51 -0400

    JungleCook
    An awful attempt at creating the Jungle Cook final. My mental health has tanked and I can no longer think straight but I did whatever I could figure out.

---
+++ jungleCook/public/app/app.js
@@ -0,0 +1,47 @@
+function initNav(){
+
+    $("a").click(function(e){
+        var cp = PROVIDER.getCurrentPageName();
+        SAAD_UTILITY.trace("initNav ", cp);
+        var btnId = e.currentTarget.id;
+
+        if(cp!= btnId){
+            loadContent(btnId);
+        }
+
+        
+    });
+}
+
+function loadContent(pageName){
+
+    var pageContent = PROVIDER.getPageContent(pageName);
+    SAAD_UTILITY.trace("app.js line 14", pageContent);
+    $(".content").html(pageContent);
+
+    initNav();
+}
+
+function populateNav(){
+    
+
+    var nav = PROVIDER.getMainNav();
+
+    $.each(nav, function(idx, link){
+        //console.log(link);
+        $("nav").append(`<a id="${link.path}" href="#">${link.linkName}</a>`);
+    });
+
+    loadContent("home");
+
+}
+
+function dataIsLoaded(){
+    //console.log("loaded");
+    populateNav();
+}
+
+$(document).ready(function(){
+   // alert('Hello'); 
+   PROVIDER.getData(dataIsLoaded);
+});
